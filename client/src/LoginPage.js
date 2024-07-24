04955e7cadf0a6fc4eb4866ca1eadcdb5b7a34a7
Othytou <othmane@olcr.fr>
2024-07-24 15:45:01 +0200

    Login update

--- client/src/LoginPage.js
+++ client/src/LoginPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 import "./LoginPage.css";
 
 function LoginPage({ onLogin }) {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLoginClick = () => {
-    onLogin();
-    navigate("/");
+  const handleLoginClick = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        console.log('Login successful');
+        onLogin();
+        navigate("/");
+      } else {
+        console.error('Login failed');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
   };
 
   const handleConnexionClick = () => {
@@ -28,11 +46,23 @@ function LoginPage({ onLogin }) {
         >
           <div className="form-group">
             <label htmlFor="username">Identifiant :</label>
-            <input type="text" id="username" required />
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Mot de passe :</label>
-            <input type="password" id="password" required />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           <button type="submit" className="submit-button">
             Connexion
