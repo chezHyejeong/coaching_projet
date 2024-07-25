fc97ddc017795ad8394f4ebc3aa766b57fcc9cb2
Hyejeong Im <hjl0311@gmail.com>
2024-07-25 15:22:25 +0200

    update view all reservations page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import CarDetailsPage from "./CarDetailsPage";
 import carImage from "./assets/images/gate_car1.png";
 import ProfilePage from "./ProfilePage";
 import ManageCarsPage from "./ManageCarsPage";
+import ViewAllReservationsPage from "./ViewAllReservationsPage";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -39,6 +40,10 @@ function App() {
           <Route path="/car/:id" element={<CarDetailsPage cars={carData} />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/manage-cars" element={<ManageCarsPage />} />
+          <Route
+            path="/view-all-reservations"
+            element={<ViewAllReservationsPage />}
+          />
         </Routes>
       </div>
     </Router>
